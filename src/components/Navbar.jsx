49ebc2a08a93fa36c399b4706b818562973e6f38
Sadman Arif Wamim <sadmanarifwamim11@gmail.com>
2024-02-18 18:48:47 +0600

    Added Section Wrapper

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-20 h-20 object-contain' />
+          <img src={logo} alt='logo' className='w-10 h-10 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Sadman Arif Wamim &nbsp;
             <span className='sm:block hidden'>| Software Developer</span>
